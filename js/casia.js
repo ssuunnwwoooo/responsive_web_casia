b9cee9c0b46cb69c77d2fa92d30270f49e96332b
sunwoo <133613205+ssuunnwwoooo@users.noreply.github.com>
2023-09-01 11:59:51 +0900

    Add files via upload

--- js/casia.js
+++ js/casia.js
@@ -1,5 +1,15 @@
 $(function () {
 
+
+    $('.mopen').on('click', function () {
+        $(this).toggleClass('on');
+        $('.gnb').toggleClass('on');
+    });
+
+    $('.gnb').on('wheel touchmove', function (e) {
+        e.preventDefault();
+    });
+
     $(window).on('scroll', function () {
         let sct = $(window).scrollTop();
         if (sct > 0) {
